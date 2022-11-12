db5072906c896b70f258ff2de8015cc6d07fde1d
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2022-11-12 11:25:45 +0500

    Class

---
+++ app.js
@@ -0,0 +1,73 @@
+// var number1=+prompt("Enter a number1");
+// var number2=+prompt("Enter a number2");
+
+
+// function add(number1,number2){
+//     var result=number1+number2;
+//     return result;
+// }
+
+// function subtract(number1,number2){
+//     var result=number1-number2;
+//     return result;
+// }
+
+
+// // var answer=add(number1,number2);
+// // document.write(answer);
+
+// var sub=subtract(number1,number2);
+// document.write(subtract);
+
+// var person={
+//     "Name":"Sameer",
+//     "Name1":"kjhb",
+//     "Age":23,
+//     "Email":"someone@example.com"
+// }
+
+// document.write(person.)
+
+
+// var student = { 
+//     name: 'John',
+//     age: 20,
+//     hobbies: ['reading', 'games', 'coding'],
+// };
+
+// // using for...in
+// for (var key in student) { 
+//     var value;
+
+//     // get the value
+//     value = student[key];
+
+//     document.write(value); 
+// }
+
+
+// program to check if the string is palindrome or not
+
+// function checkPalindrome(string) {
+
+//     // find the length of a string
+//     const len = string.length;
+
+//     // loop through half of the string
+//     for (let i = 0; i < len / 2; i++) {
+
+//         // check if first and last string are same
+//         if (string[i] !== string[len -1 - i]) {//string[4]
+//             return 'It is not a palindrome';
+//         }
+//     }
+//     return 'It is a palindrome';
+// }
+
+// // take input
+// const string = prompt('Enter a string: ');
+
+// // call the function
+// const value = checkPalindrome(string);
+
+// document.write(value);
